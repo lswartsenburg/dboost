a3719d177a4b4ae87d55491f864b7f92db65a5bd
Lucas Swartsenburg <lucas@optimizely.com>
2018-09-10 00:54:22 -0700

    Changing the db argument

--- www/settings.py
+++ www/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'www.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-db_from_env = dj_database_url.config(conn_max_age=500, require_ssl=True)
+db_from_env = dj_database_url.config(conn_max_age=500, ssl_require=True)
 
 DATABASES = {
     'default': {
