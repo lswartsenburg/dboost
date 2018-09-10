3285a83530a386d2a744f35f040847afd736d163
Lucas Swartsenburg <lucas@optimizely.com>
2018-09-10 00:39:49 -0700

    Changing the module from hello to app

--- www/urls.py
+++ www/urls.py
@@ -10,6 +10,6 @@ import app.views
 
 urlpatterns = [
     url(r'^$', app.views.index, name='index'),
-    url(r'^db', hello.views.db, name='db'),
+    url(r'^db', app.views.db, name='db'),
     path('admin/', admin.site.urls),
 ]
